74643cf7aab58096a21e0fdf4aea90a1ed8479c9
HappyLemon <sm.itsmyworld@gmail.com>
2024-04-21 17:24:48 +0500

    template

---
+++ Tests/GlobalUsings.cs
@@ -0,0 +1,7 @@
+global using RestSharp;
+global using NLog;
+
+global using System.Diagnostics;
+global using System.Net;
+global using System.Text.Json;
+global using System.Text.Json.Serialization;
