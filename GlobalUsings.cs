4091732adeb353f7ca0e74250be20378d9e26957
EvgeniiSmirnov <EvgeniiSmirnov@users.noreply.github.com>
2024-04-23 01:47:03 +0000

    deploy: ac2cbd5ec05830d05313bf10866eb1c3f97dabb9

--- GlobalUsings.cs
+++
@@ -1,7 +0,0 @@
-global using RestSharp;
-global using NLog;
-
-global using System.Diagnostics;
-global using System.Net;
-global using System.Text.Json;
-global using System.Text.Json.Serialization;
